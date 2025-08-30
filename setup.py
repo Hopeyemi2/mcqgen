b8d10fcf01e375e4bcde4bea0365532e6cc3132c
Hopeyemi2 <afeez.ojolowo.opeyemi@gmail.com>
2025-08-30 12:36:13 -0400

    File Updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='afeez ojolowo',
+    author_email="afeez.ojolowo.opeyemi@gmail.com",
+    install_requires=["openai", "langchain", "streamlit", "python-dotenv", "pyPDF2" ],
+    packages=find_packages()
+)
